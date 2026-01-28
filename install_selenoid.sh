68dbe68caf15ad40a0372bcb187ab20cf3acdde0
Saintdizen <51364739+Saintdizen@users.noreply.github.com>
2026-01-28 14:02:30 +0300

    Update install_selenoid.sh

--- install_selenoid.sh
+++ install_selenoid.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 # Ubuntu
 # Обновление системы
-sudo apt update
-sudo apt upgrade
+sudo apt-get update
 # Установка DOCKER
-sudo apt install pt-transport-https ca-certificates curl software-properties-common wget net-tools
+sudo apt-get install pt-transport-https ca-certificates curl software-properties-common wget net-tools
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-sudo apt update -y
-sudo apt install docker-ce=5:19.03.9~3-0~ubuntu-focal
+sudo apt-get update
+sudo apt-get install docker-ce=5:19.03.9~3-0~ubuntu-focal
 # Фикс x Failed to start: failed to start container: Error response from daemon: cgroups: cgroup mountpoint does not exist: unknown
 sudo mkdir -p /sys/fs/cgroup/systemd
 sudo mount -t cgroup -o none,name=systemd cgroup /sys/fs/cgroup/systemd
 # Установка SELENOID
 cd ~
-rm -rf $(ls)
 wget https://github.com/aerokube/cm/releases/download/1.8.8/cm_linux_amd64
 chmod +x cm_linux_amd64
 sudo ./cm_linux_amd64 selenoid start --vnc
